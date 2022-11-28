09859db1fb2dbbffe0e7b525bea0e9046fb912bb
lubin <lubin@xiaoduotech.com>
2022-11-28 20:02:17 +0800

    study

--- miniprogram/pages/wxml/index.ts
+++ miniprogram/pages/wxml/index.ts
@@ -5,7 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-    time: (new Date()).toString(),
+    // item:{
+    //   time: "2022-11-28",
+    //   msg: "this is a template",
+    //   index: 0,
+    // },
+    time: "2022-11-28",
+    msg: "this is a template",
+    index: 0,
     var2: undefined,
     var3: null,
     var4: "var4",
@@ -13,9 +20,50 @@ Page({
     b : 2,
     c : 3,
     name: "world",
-    length: 10
+    length: 10,
+    array :[
+      {
+        message: 'foo'
+      },{
+        message: 'bar'
+      }
+    ],
+    objectArray: [
+      {id: 5, unique: 'unique_5'},
+      {id: 4, unique: 'unique_4'},
+      {id: 3, unique: 'unique_3'},
+      {id: 2, unique: 'unique_2'},
+      {id: 1, unique: 'unique_1'},
+      {id: 0, unique: 'unique_0'},
+    ],
+    numberArray: [1, 2, 3, 4]
   },
-
+  switch: function() {
+    const length = this.data.objectArray.length
+    for (let i = 0; i < length; ++i) {
+      const x = Math.floor(Math.random() * length)
+      const y = Math.floor(Math.random() * length)
+      const temp = this.data.objectArray[x]
+      this.data.objectArray[x] = this.data.objectArray[y]
+      this.data.objectArray[y] = temp
+    }
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+  },
+  addToFront: function() {
+    const length = this.data.objectArray.length
+    this.data.objectArray = [{id: length, unique: 'unique_' + length}].concat(this.data.objectArray)
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+  },
+  addNumberToFront: function(){
+    this.data.numberArray = [ this.data.numberArray.length + 1 ].concat(this.data.numberArray)
+    this.setData({
+      numberArray: this.data.numberArray
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
